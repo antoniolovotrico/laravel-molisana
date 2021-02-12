e829a3df5df62e024436fc0a41d03b3f15231303
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-12 13:50:20 +0100

    moved database pasta to confing folder creating a new file called data_pasta.php, remade the array in json instead of php,and decode it in root product return, changed also the foreach path to show the element of array that now are objects

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,42 +3,42 @@
 @section('main_content')
     <h2>LE LUNGHE</h2>
     <div class="lunghe_container">
-        @foreach ($data as $item)
-        @if ($item['tipo'] == "lunga")
+        @foreach ($dati as $item)
+        @if ($item -> tipo == "lunga")
         <div class="card">
             <div class="overlay"></div>
             <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-            <h3>{{ $item['titolo'] }}</h3>
-            <p>Cottura:{{ $item['cottura'] }}</p>
-            <img src="{{ $item['src'] }}" alt="">
+            <h3>{{ $item -> titolo }}</h3>
+            <p>Cottura:{{ $item -> cottura }}</p>
+            <img src="{{ $item -> src }}" alt="">
         </div>
         @endif
         @endforeach
     </div>
     <h2>LE CORTE</h2>
     <div class="corte_container">
-        @foreach ($data as $item)
-        @if ($item['tipo'] == "corta")
+        @foreach ($dati as $item)
+        @if ($item -> tipo == "corta")
         <div class="card">
             <div class="overlay"></div>
             <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-            <h3>{{ $item['titolo'] }}</h3>
-            <p>Cottura:{{ $item['cottura'] }}</p>
-            <img src="{{ $item['src'] }}" alt="">
+            <h3>{{ $item -> titolo }}</h3>
+            <p>Cottura:{{ $item -> cottura }}</p>
+            <img src="{{ $item -> src }}" alt="">
         </div>
         @endif
         @endforeach
     </div>
     <h2>LE CORTISSIME</h2>
     <div class="cortissime_container">
-        @foreach ($data as $item)
-        @if ($item['tipo'] == "cortissima")
+        @foreach ($dati as $item)
+        @if ($item -> tipo == "cortissima")
         <div class="card">
             <div class="overlay"></div>
             <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-            <h3>{{ $item['titolo'] }}</h3>
-            <p>Cottura:{{ $item['cottura'] }}</p>
-            <img src="{{ $item['src'] }}" alt="">
+            <h3>{{ $item -> titolo }}</h3>
+            <p>Cottura:{{ $item -> cottura }}</p>
+            <img src="{{ $item -> src }}" alt="">
         </div>
         @endif
         @endforeach
