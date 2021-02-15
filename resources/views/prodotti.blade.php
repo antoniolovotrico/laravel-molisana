80b867d6f508ba28c9c414e4ceaf34a4e140e90c
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-15 03:16:36 +0100

    set all the content in main section of prodotti.blade.php in a single foreach using  property to give unique title to every section

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,52 +3,25 @@
 prodotti
 @endsection
 @section('main_content')
-    <h2>LE LUNGHE</h2>
+
     <div class="lunghe_container">
         @foreach ($dati as $key => $item)
-        @if ($item['tipo'] == "lunga")
-        <div class="card">
-            <div class="overlay"></div>
-            <img src="{{ $item['src'] }}" alt="" class="background">
-            <a href="{{ route('prodotto', $key) }}">
-                <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-                <h3>{{ $item['titolo'] }}</h3>
-                <p>Cottura:{{ $item['cottura'] }}</p>
-            </a>      
-        </div>
-        @endif
-        @endforeach
-    </div>
-    <h2>LE CORTE</h2>
-    <div class="corte_container">
-        @foreach ($dati as $key => $item)
-        @if ($item['tipo'] == "corta")
-        <div class="card">
-            <div class="overlay"></div>
-            <img src="{{ $item['src'] }}" alt="" class="background">
-            <a href="{{ route('prodotto', $key) }}">
-                <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-                <h3>{{ $item['titolo'] }}</h3>
-                <p>Cottura:{{ $item['cottura'] }}</p>
-            </a>      
-        </div>
-        @endif
-        @endforeach
-    </div>
-    <h2>LE CORTISSIME</h2>
-    <div class="cortissime_container">
-        @foreach ($dati as $key => $item)
-        @if ($item['tipo'] == "cortissima")
-        <div class="card">
-            <div class="overlay"></div>
-            <img src="{{ $item['src'] }}" alt="" class="background">
-            <a href="{{ route('prodotto', $key) }}">
-                <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
-                <h3>{{ $item['titolo'] }}</h3>
-                <p>Cottura:{{ $item['cottura'] }}</p>
-            </a>      
-        </div>
-        @endif
+            @if ($loop -> first)
+            <h2>{{ $item['tipo'] }}</h2>   
+            @elseif ($loop -> index == 5)
+            <h2>{{ $item['tipo'] }}</h2> 
+            @elseif ($loop -> index == 11)
+            <h2>{{ $item['tipo'] }}</h2>
+            @endif
+            <div class="card">
+                <div class="overlay"></div>
+                <img src="{{ $item['src'] }}" alt="" class="background">
+                <a href="{{ route('prodotto', $key) }}">
+                    <img class="svg" src="{{ asset('img/icon.svg') }}" alt="">
+                    <h3>{{ $item['titolo'] }}</h3>
+                    <p>Cottura:{{ $item['cottura'] }}</p>
+                </a>      
+            </div>   
         @endforeach
     </div>
 @endsection
